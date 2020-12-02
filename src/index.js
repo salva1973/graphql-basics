57a1702cf73fa573d8abe29002b2237df7389513
Salvatore Vivolo <salvatore.vivolo@gmail.com>
2020-12-02 18:21:44 +0100

    added relationships

--- src/index.js
+++ src/index.js
@@ -21,28 +21,57 @@ const users = [{
 
 // Demo posts data
 const posts = [{
-    id: '1',
+    id: '10',
     title: 'GraphQL 101',
     body: 'Best course ever',
-    published: true
+    published: true,
+    author: '1'
 }, {
-    id :'2',
+    id :'11',
     title: 'SwiftUI 101',
     body: 'To be completed',
-    published: false
+    published: false,
+    author: '1'
 },
 {
-    id :'3',
+    id :'12',
     title: 'MongoDB 101',
     body: 'To be completed',
-    published: false
+    published: false,
+    author: '2'
 }]
+
+// Demo comments data
+const comments = [{
+    id: '100',
+    text: 'Fantastic course!',
+    author: '1',
+    post: '10'
+}, {
+    id :'101',
+    text: 'Terrific course!',
+    author: '1',
+    post: '10'
+}, {
+    id :'102',
+    text: 'Very good introduction!',
+    author: '2',
+    post: '11'
+}, {
+    id :'103',
+    text: 'Not so good...',
+    author: '3',
+    post: '12'
+}
+
+]
 
 // Type definitions (application schema)
 const typeDefs = `
      type Query {
          users(query: String): [User!]!
          posts(query: String): [Post!]!
+         comments(query: String): [Comment!]!
          me: User!
          post: Post!
      }
@@ -52,6 +81,8 @@ const typeDefs = `
          name: String!
          email: String!
          age: Int
+         posts: [Post!]!
+         comments: [Comment!]!
      }
 
      type Post {
@@ -59,7 +90,16 @@ const typeDefs = `
          title: String!
          body: String!
          published: Boolean!
+         author: User!
+         comments: [Comment!]!
      }
+
+     type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
+    }
 `
 
 // Resolvers for the API
@@ -87,6 +127,17 @@ const resolvers = {
             })
 
         },
+        comments(parent, args, ctx, info) {
+            if (!args.query) {
+                return comments
+            }
+
+            return comments.filter((comment) => {
+                const isTextMatch = comment.text.toLowerCase().includes(args.query.toLowerCase())
+                return isTextMatch
+            })
+
+        },
         me() {
             return {
                 id: '390800',
@@ -103,6 +154,42 @@ const resolvers = {
                 body: 'Nice course',
                 published: true
             }
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            }) 
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.author === parent.id
+            })
         }
     }
 }
